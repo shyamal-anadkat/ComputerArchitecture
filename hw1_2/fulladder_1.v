455d03da727b716b915c8cf81a4040f933814d55
zostale <shyamalanadkat@gmail.com>
2017-01-27 12:47:12 -0600

    SA 1/27 P1 and P2 with testbench

--- hw1_2/fulladder_1.v
+++ hw1_2/fulladder_1.v
@@ -1,12 +1,12 @@
 module fulladder_1 (
 	input A, B, Cin,
 	output S, Cout);
-	wire abx,abn,abx2;
+	wire xorab,nandab,xor2;
 	
-	xor2 x1(.in1(A),.in2(B),.out(abx));
-	xor2 x2(.in1(abx),.in2(Cin),.out(S));
+	xor2 x1(.in1(A),.in2(B),.out(xorab));
+	xor2 x2(.in1(xorab),.in2(Cin),.out(S));
 
-	nand2 n1(.in1(A),.in2(B), .out(abn));
-	nand2 n2(.in1(abx),.in2(Cin),.out(abx2));
-	nand2 n3(.in1(abx2),.in2(abn),.out(Cout));
+	nand2 n1(.in1(A),.in2(B), .out(nandab));
+	nand2 n2(.in1(xorab),.in2(Cin),.out(xor2));
+	nand2 n3(.in1(xor2),.in2(nandab),.out(Cout));
 endmodule
