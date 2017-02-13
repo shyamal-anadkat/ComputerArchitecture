b63d479bf846fb644030715cbd9c7f8cec5f789c
SANJAY Rajmohan <rajmohan@royal-16.cs.wisc.edu>
2017-02-12 19:47:08 -0600

    All tests passed

--- HW2_files/hw2_2/xor2.v
+++ HW2_files/hw2_2/xor2.v
@@ -1,5 +1,13 @@
 module xor2(a, b, out);
 	input a, b; 
 	output out; 
-	assign out = a ^ b;
+	wire invA, invB, out1, out2;
+
+	not1 not_a (.in1(a), .out(invA));
+	not1 not_b (.in1(b), .out(invB));
+
+	and2 a1 (.a(a), .b(invB), .out(out1));
+	and2 a2 (.a(invA), .b(b), .out(out2));
+
+	or2 o1 (.a(out1), .b(out2), .out(out));
 endmodule 
