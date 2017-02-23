bdccd549f5227a1f8fa96a8999e85aeea35d9498
zostale <shyamalanadkat@gmail.com>
2017-02-22 20:18:15 -0600

    SA 2/22 part 3 all tests PASSING

--- HW3_files/hw3_3/rf_bypass.v
+++ HW3_files/hw3_3/rf_bypass.v
@@ -1,6 +1,8 @@
 /* $Author: karu $ */
 /* $LastChangedDate: 2009-03-04 23:09:45 -0600 (Wed, 04 Mar 2009) $ */
 /* $Rev: 45 $ */
+// SAnadkat Rajmohan 2/22
+
 module rf_bypass (
            // Outputs
            read1data, read2data, err,
@@ -18,7 +20,37 @@ module rf_bypass (
    output [15:0] read2data;
    output        err;
 
-   // your code
+   wire bp1, bp2;
+   wire [15:0] read1, read2;
+   wire comp_out1, comp_out2; 
+
+   //instantiate RegFile
+   rf rfmod(
+          // Outputs
+          .read1data                    (read1),
+          .read2data                    (read2),
+          .err                          (err),
+          // Inputs
+          .clk                          (clk),
+          .rst                          (rst),
+          .read1regsel                  (read1regsel),
+          .read2regsel                  (read2regsel),
+          .writeregsel                  (writeregsel),
+          .writedata                    (writedata),
+          .write                        (write));
+
+  
+  //bp1 = 1 if read1sel = writeregsel && write
+  //bp2 = 1 if read2sel = writeregsel && write 
+
+  comparator3 cmp1(.a(read1regsel), .b(writeregsel), .out(comp_out1));
+  comparator3 cmp2(.a(read2regsel), .b(writeregsel), .out(comp_out2));
+
+  and2 a1(.a(comp_out1),.b(write),.out(bp1));
+  and2 a2(.a(comp_out2),.b(write),.out(bp2));
+
+  //select between write and read1
+  mux2_1 bypass1mod [15:0] (.InA(read1),.InB(writedata),.S(bp1),.Out(read1data));
+  mux2_1 bypass2mod [15:0] (.InA(read2),.InB(writedata),.S(bp2),.Out(read2data));
 
 endmodule
-// DUMMY LINE FOR REV CONTROL :1:
