43e9c4adc2b37184bbcc9b658783d220e9ffa744
zostale <shyamalanadkat@gmail.com>
2017-04-12 19:30:33 -0500

    hw5 work

--- HW5_files/hw5_1/p1.asm
+++ HW5_files/hw5_1/p1.asm
@@ -1,1 +1,5 @@
 // Write your assembly program for Problem 1 (a) here.
+lbi  r1, 10
+add  r1, r3, r2
+andn r2, r1, r3
+halt
--- HW5_files/hw5_2/p2.asm
+++ HW5_files/hw5_2/p2.asm
@@ -1,1 +1,7 @@
 // Write your assembly program for Problem 2 (a) here.
+lbi r3, 0
+beqz r3, 1
+and r1, r2, r3
+add r2, r2, r2
+nop 
+halt
